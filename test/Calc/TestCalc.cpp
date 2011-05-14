f47a5c87f75ce5c5e729ec8f4b3f78930afab0fd
voidy21 <y2.void+github@gmail.com>
2011-05-15 01:43:13 +0900

    CaptureStdoutなんてあるんですね

--- test/Calc/TestCalc.cpp
+++ test/Calc/TestCalc.cpp
@@ -1,8 +1,21 @@
 /**
  * @file TestCalc.cpp
  */
-#include <gtest/gtest.h>
-#include "Calc.h"
+#include <gtest/gtest.h> 
+#include <gmock/gmock.h> 
+#include <gtest/internal/gtest-port.h> 
+#include "Calc.h" 
+#include <iostream>
+using ::testing::internal::CaptureStdout;
+using ::testing::internal::GetCapturedStdout;
+using ::testing::Invoke;
+using ::testing::_;
+
+class MockCalc : public Calc
+{
+public:
+  MOCK_METHOD0(throw_exception, void());
+};
 
 /**
  * @see Calc
@@ -11,11 +24,11 @@
 class TestCalc : public ::testing::Test
 {
 public:
-  Calc* calc;
+  MockCalc* calc;
 
   virtual void SetUp()
   {
-    calc  = new Calc();
+    calc  = new MockCalc();
   }
 
   virtual void TearDown()
@@ -36,5 +49,14 @@ public:
 TEST_F(TestCalc, 1_1_1)
 {
   EXPECT_EQ(3, calc->add(1, 2));
+}
+
+TEST_F(TestCalc, 1_2_1)
+{
+  std::cout << typeid(calc).name() << std::endl;
+
+  CaptureStdout();
+  std::cout << "hoge" << std::endl;
+  EXPECT_STREQ("hoge\n", GetCapturedStdout().c_str());
 }
 
